f9e892e4b397dd7fd0ee5fe7da48b8d677eaff69
Norbert Melzer <timmelzer@gmail.com>
2022-09-11 13:35:16 +0200

    chore: update nixpkgs

--- .nix/beams.nix
+++ .nix/beams.nix
@@ -1,13 +1,5 @@
 pkgs: let
   beamPkgs = with pkgs.beam_nox; packagesWith interpreters.erlangR25;
   erlang = beamPkgs.erlang;
-  elixir = beamPkgs.elixir_1_13.overrideAttrs (oa: rec {
-    version = "1.14.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "elixir-lang";
-      repo = "elixir";
-      rev = "v${version}";
-      hash = "sha256-NJQ2unK7AeLGfaW/hVXm7yroweEfudKVUa216RUmLJs=";
-    };
-  });
+  elixir = beamPkgs.elixir_1_14;
 in {inherit erlang elixir;}
